c9bd263328985cc609a188dceedbe384da3d7ffb
Your Name <rdcvithanacodeolima@gmail.com>
2024-07-29 16:06:29 +0530

    robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,23 +1,23 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: 'https://rdcvithana.com',
-  generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  generateIndexSitemap: true, // (optional) Generate a sitemap index file
-  sitemapSize: 5000, // (optional) Maximum number of URLs per sitemap file
-  changefreq: 'daily', // (optional) How often the pages are updated
-  priority: 0.7, // (optional) Default priority for pages
-  exclude: ['/admin/**', '/private/**'], // (optional) Exclude specific paths from the sitemap
+  generateRobotsTxt: true, // Generate a robots.txt file
+  generateIndexSitemap: true, // Generate a sitemap index file
+  sitemapSize: 5000, // Maximum number of URLs per sitemap file
+  changefreq: 'daily', // How often the pages are updated
+  priority: 0.7, // Default priority for pages
+  exclude: ['/admin/**', '/private/**'], // Exclude specific paths from the sitemap
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
+        disallow: ['/api/', '/_next/', '/static/', '/private/'],
         allow: '/',
-        disallow: ['/private', '/admin'],
       },
     ],
     additionalSitemaps: [
       'https://rdcvithana.com/sitemap.xml',
+      'https://rdcvithana.com/sitemap-0.xml',
     ],
   },
 };
