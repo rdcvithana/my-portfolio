717848d75b61c4a2872a2761e4fff8b8d9e2556b
Your Name <rdcvithanacodeolima@gmail.com>
2024-07-30 10:15:15 +0530

    cc

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: 'https://rdcvithana.com',
-  generateIndexSitemap: true, // Generate a sitemap index file
-  sitemapSize: 5000, // Maximum number of URLs per sitemap file
-  changefreq: 'daily', // How often the pages are updated
-  priority: 0.7, // Default priority for pages
-  exclude: ['/admin/**', '/private/**'], // Exclude specific paths from the sitemap
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  generateIndexSitemap: true, // (optional) Generate a sitemap index file
+  sitemapSize: 5000, // (optional) Maximum number of URLs per sitemap file
+  changefreq: 'daily', // (optional) How often the pages are updated
+  priority: 0.7, // (optional) Default priority for pages
+  exclude: ['/admin/**', '/private/**'], // (optional) Exclude specific paths from the sitemap
   robotsTxtOptions: {
     policies: [
       {
